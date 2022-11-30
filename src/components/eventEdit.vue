6de3bb365e6b713a207d40e4eaadcffeac99d51e
itsmeny <chokjaroenp501@gmail.com>
2022-12-01 03:45:07 +0700

    event edit improvement

--- src/components/eventEdit.vue
+++ src/components/eventEdit.vue
@@ -47,21 +47,22 @@ const checkTimeSlot = async (date, eventCategoryId) => {
   slot.value.length = 0;
   const slot_t = await useEvent.getTime(date, eventCategoryId);
   slot_t.forEach((x) => {
-    slot.value.push(new Date(x.eventStartTime).toString());
+    if (x.eventStartTime != d_tmp.value) {
+      slot.value.push(new Date(x.eventStartTime).toString());
+    }
   });
 };
 
 const timeTable = ref([]);
 
 const generateTimeSlot = (eventDuration) => {
-  console.log(eventDuration);
   timeTable.value.length = 0;
   d = new Date(
     startTime.value.split("-")[0] +
-      "-" +
-      startTime.value.split("-")[1] +
-      "-" +
-      startTime.value.split("-")[2]
+    "-" +
+    startTime.value.split("-")[1] +
+    "-" +
+    startTime.value.split("-")[2]
   );
   d.setHours(0, 0, 0, 0);
   for (let i = 0; i < 1440 / (eventDuration + 5); i++) {
@@ -69,6 +70,13 @@ const generateTimeSlot = (eventDuration) => {
       getTime(d) + " - " + getTime((d = addMinutes(d, eventDuration)))
     );
     d = addMinutes(d, 5);
+  }
+  var active_date = new Date(props.eventz.eventStartTime);
+  for (let i = 0; i < timeTable.value.length; i++) {
+    if (timeTable.value[i].includes(active_date)) {
+      activeIndex.value = i;
+      time.value = timeTable.value[i].split('-')[0].trim();
+    }
   }
   timeTable.value.splice(timeTable.value.length - 1, 1);
 };
@@ -88,7 +96,7 @@ const editEvent = async () => {
     eventStartTime: d_tmp.value,
     eventNotes: eNotes.value,
     attachment:
-      fileName.value.length > 0
+      fileName.value != null && fileName.value.length > 0
         ? fileName.value == props.eventz.attachment
           ? fileName.value
           : timestamp + "," + fileName.value
@@ -180,32 +188,26 @@ const clearFile = () => {
 onBeforeMount(() => {
   console.log(props.eventz.attachment);
   console.log(fileName.value);
+  startTime.value = props.eventz.eventStartTime.split('T')[0];
+  d_tmp.value = props.eventz.eventStartTime;
+  tryCall();
 })
+
 </script>
 <template>
   <div>
     <section class="py-4 py-xl-5" style="background: #ffffff">
       <!-- modal noti -->
       <Transition>
-        <section
-          class="border bottom-dark"
-          id="resFile"
-          style="background: #0071e3"
-          v-show="resFiles"
-        >
+        <section class="border bottom-dark" id="resFile" style="background: #0071e3" v-show="resFiles">
           <nav class="navbar navbar-light" style="margin: 2px">
             <div class="px-5 container align-items-center">
               <h6 class="fw-bold px-5 mt-2" style="color: #ffffff">
                 เอกสารของคุณมีขนาดใหญ่เกิน 10 MB
               </h6>
               <ul class="navbar-nav ms-auto">
-                <button
-                  type="button"
-                  class="btn-close px-5"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                  @click="resFiles = false"
-                ></button>
+                <button type="button" class="btn-close px-5" data-bs-dismiss="modal" aria-label="Close"
+                  @click="resFiles = false"></button>
               </ul>
             </div>
           </nav>
@@ -216,8 +218,7 @@ onBeforeMount(() => {
         <div class="row gy-4 gy-md-0">
           <div
             class="col-md-6 text-center text-md-start d-flex d-sm-flex d-md-flex justify-content-center align-items-center justify-content-md-start align-items-md-center justify-content-xl-center"
-            style="margin: 60px"
-          >
+            style="margin: 60px">
             <div style="max-width: 350px">
               <h2 class="text-uppercase fw-bold">แก้ไขการนัดหมาย</h2>
               <p class="my-3">
@@ -233,18 +234,10 @@ onBeforeMount(() => {
         <div class="row gy-4 gy-md-0 justify-content-center">
           <div class="col-md-3 p-3">
             <div class="form-check">
-              <input
-                class="form-check-input"
-                type="radio"
-                name="flexRadioDefault"
-                disabled
-                checked
-              />
+              <input class="form-check-input" type="radio" name="flexRadioDefault" disabled checked />
               {{ eventz.eventCategory.eventCategoryName }}
               <label class="form-check-label">
-                <small class="text-muted"
-                  >({{ eventz.eventCategory.eventDuration }} นาที)</small
-                >
+                <small class="text-muted">({{ eventz.eventCategory.eventDuration }} นาที)</small>
               </label>
             </div>
           </div>
@@ -253,21 +246,19 @@ onBeforeMount(() => {
       <div class="container py-4 py-xl-5">
         <div class="row gy-4 gy-md-0">
           <div
-            class="col-md-3 text-center text-md-start d-flex d-sm-flex d-md-flex justify-content-center align-items-center justify-content-md-start align-items-md-center justify-content-xl-center"
-          ></div>
+            class="col-md-3 text-center text-md-start d-flex d-sm-flex d-md-flex justify-content-center align-items-center justify-content-md-start align-items-md-center justify-content-xl-center">
+          </div>
           <div class="col-md-6 form-floating">
             <form class="mt-1">
               <div class="row">
                 <div class="col">
                   <p>
-                    <strong>ชื่อ: </strong
-                    >{{ eventz.bookingName.split(" ")[0] }}
+                    <strong>ชื่อ: </strong>{{ eventz.bookingName.split(" ")[0] }}
                   </p>
                 </div>
                 <div class="col">
                   <p>
-                    <strong>นามสกุล: </strong
-                    >{{ eventz.bookingName.split(" ")[1] }}
+                    <strong>นามสกุล: </strong>{{ eventz.bookingName.split(" ")[1] }}
                   </p>
                 </div>
               </div>
@@ -284,78 +275,43 @@ onBeforeMount(() => {
         <div class="row gy-4 gy-md-0">
           <div class="col-md-6">
             <div class="m-5">
-              <input
-                type="date"
-                class="form-control"
-                v-model="startTime"
-                required
-                :min="getCurrDate()"
-                @change="tryCall()"
-              />
+              <input type="date" class="form-control" v-model="startTime" required :min="getCurrDate()"
+                @change="tryCall()" />
             </div>
             <div>
-              <div
-                class="container text-center"
-                v-show="startTime.length > 0 && getCurrDate() <= startTime"
-              >
+              <div class="container text-center" v-show="startTime.length > 0 && getCurrDate() <= startTime">
                 <div class="row row-cols-5 list-group list-group-item">
-                  <button
-                    type="button"
-                    v-for="(x, index) in timeTable"
-                    :key="index"
-                    @click="
-                      time = timeTable[index].split('-')[0].trim();
-                      activeIndex = index;
-                      activeClick(index);
-                      d_tmp = new Date(x.split('-')[0].trim()).toISOString();
-                    "
-                    :class="activeClick(index)"
-                    :disabled="
-                      slot.includes(x.split('-')[0].trim()) ||
-                      new Date(x.split('-')[0].trim()) < new Date()
-                    "
-                    :activeIndex="index"
-                    class="'btn-sm'"
-                  >
+                  <button type="button" v-for="(x, index) in timeTable" :key="index" @click="
+  time = timeTable[index].split('-')[0].trim();
+activeIndex = index;
+activeClick(index);
+d_tmp = new Date(x.split('-')[0].trim()).toISOString();
+                  " :class="activeClick(index)" :disabled="
+                    slot.includes(x.split('-')[0].trim()) ||
+                    new Date(x.split('-')[0].trim()) < new Date()
+                  " :activeIndex="index" class="'btn-sm'">
                     {{
-                      new Date(x.split("-")[0])
-                        .toLocaleTimeString("it-IT")
-                        .substring(0, 5)
+                        new Date(x.split("-")[0])
+                          .toLocaleTimeString("it-IT")
+                          .substring(0, 5)
                     }}
                     -
                     {{
-                      new Date(x.split("-")[1])
-                        .toLocaleTimeString("it-IT")
-                        .substring(0, 5)
+                        new Date(x.split("-")[1])
+                          .toLocaleTimeString("it-IT")
+                          .substring(0, 5)
                     }}
-                    <small v-if="slot.includes(x.split('-')[0].trim())"
-                      >เวลานี้ถูกจองแล้ว</small
-                    >
-                    <small v-if="new Date(x.split('-')[0].trim()) < new Date()"
-                      >หมดเวลาจอง</small
-                    >
+                    <small v-if="(slot.includes(x.split('-')[0].trim()))">เวลานี้ถูกจองแล้ว</small>
+                    <small v-if="new Date(x.split('-')[0].trim()) < new Date()">หมดเวลาจอง</small>
                   </button>
                 </div>
               </div>
               <div class="col m-5">
-                <textarea
-                  rows="4"
-                  class="form-control mt-3 mb-3"
-                  placeholder="อยากบอกอะไรกับที่ปรึกษาไหม?"
-                  maxlength="300"
-                  v-model="eNotes"
-                />
+                <textarea rows="4" class="form-control mt-3 mb-3" placeholder="อยากบอกอะไรกับที่ปรึกษาไหม?"
+                  maxlength="300" v-model="eNotes" />
                 <div class="input-group">
-                  <label
-                    for="fileupload"
-                    class="form-control mt-5 file-input-area"
-                  >
-                    <input
-                      class="file-upload-input"
-                      id="fileupload"
-                      type="file"
-                      @change="sizeCheck()"
-                    />
+                  <label for="fileupload" class="form-control mt-5 file-input-area">
+                    <input class="file-upload-input" id="fileupload" type="file" @change="sizeCheck()" />
                     <div class="drag-text">
                       <p class="form-label">
                         คุณสามารถแนบเอกสารเพิ่มเติมได้ <br />
@@ -365,20 +321,17 @@ onBeforeMount(() => {
                   </label>
                 </div>
 
-                <div
-                  class="row mt-3"
-                  v-if="
-                    (props.eventz.attachment != null &&
-                      props.eventz.attachment != '') ||
-                    (fileName != null && fileName != '')
-                  "
-                >
+                <div class="row mt-3" v-if="
+                  (props.eventz.attachment != null &&
+                    props.eventz.attachment != '') ||
+                  (fileName != null && fileName != '')
+                ">
                   <div div class="col">
                     <p>
                       {{
-                        fileName.includes(",")
-                          ? fileName.split(",")[1] //work in local well
-                          : fileName
+                          fileName.includes(",")
+                            ? fileName.split(",")[1] //work in local well
+                            : fileName
                       }}
                     </p>
                     <!-- <p v-if="fileName">{{fileName}}</p>
@@ -386,20 +339,11 @@ onBeforeMount(() => {
                     <!-- <p>{{ props.eventz.attachment.split(',')[1] }}</p> -->
                   </div>
                   <div div class="col" @click="deleteFile()">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      class="bi bi-x-circle ee-ee"
-                      viewBox="0 0 16 16"
-                    >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                      class="bi bi-x-circle ee-ee" viewBox="0 0 16 16">
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                       <path
-                        d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
-                      />
-                      <path
-                        d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
-                      />
+                        d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
                     </svg>
                   </div>
                 </div>
@@ -416,22 +360,12 @@ onBeforeMount(() => {
           </div>
         </div>
         <div class="d-flex flex-row-reverse bd-highlight">
-          <button
-            class="btn btn-danger btn-sm"
-            type="button"
-            style="--bs-btn-border-radius: 1rem"
-            @click="router.push(`/Eventinfo/${props.eventz.id}`)"
-          >
+          <button class="btn btn-danger btn-sm" type="button" style="--bs-btn-border-radius: 1rem"
+            @click="router.push(`/Eventinfo/${props.eventz.id}`)">
             ยกเลิก
           </button>
-          <button
-            class="btn btn-primary btn-sm mx-4"
-            type="button"
-            data-bs-toggle="modal"
-            data-bs-target="#myModal"
-            style="--bs-btn-border-radius: 1rem"
-            :disabled="!(time != 0 && startTime != 0)"
-          >
+          <button class="btn btn-primary btn-sm mx-4" type="button" data-bs-toggle="modal" data-bs-target="#myModal"
+            style="--bs-btn-border-radius: 1rem" :disabled="!(time != 0 && startTime != 0)">
             ยืนยัน
           </button>
         </div>
@@ -444,24 +378,12 @@ onBeforeMount(() => {
         <!-- con Modal edit HTML -->
         <div class="modal-content">
           <div class="modal-header flex-column">
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-hidden="true"
-            ></button>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-hidden="true"></button>
             <div class="icon-box">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="70"
-                height="70"
-                fill="#6E6E73"
-                class="bi bi-check-lg"
-                viewBox="0 0 16 16"
-              >
+              <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="#6E6E73" class="bi bi-check-lg"
+                viewBox="0 0 16 16">
                 <path
-                  d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"
-                />
+                  d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z" />
               </svg>
             </div>
 
@@ -474,24 +396,15 @@ onBeforeMount(() => {
             </p>
           </div>
           <div class="modal-footer justify-content-center">
-            <button
-              data-bs-dismiss="modal"
-              type="button"
-              class="btn btn-primary rounded-pill"
-              data-dismiss="modal"
+            <button data-bs-dismiss="modal" type="button" class="btn btn-primary rounded-pill" data-dismiss="modal"
               @click="
-                uploadFile();
-                editEvent();
-                router.push(`/Eventinfo/${props.eventz.id}`);
-              "
-            >
+  uploadFile();
+editEvent();
+router.push(`/Eventinfo/${props.eventz.id}`);
+              ">
               ยืนยัน
             </button>
-            <button
-              type="button"
-              data-bs-dismiss="modal"
-              class="btn btn-danger rounded-pill"
-            >
+            <button type="button" data-bs-dismiss="modal" class="btn btn-danger rounded-pill">
               ยกเลิก
             </button>
           </div>
